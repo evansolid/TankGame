aa6982858dbf8a48939205f2e0ab3cc3f8201613
Evan S <105521078+evansolid@users.noreply.github.com>
2026-03-29 18:06:26 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,5 +5,7 @@ socket.on("connect", () => console.log("Connected:", socket.id));
 document.getElementById("fire").onclick = () => {
     const angle = document.getElementById("angle").value;
     const power = document.getElementById("power").value;
+    const canvas = document.getElementById("gameCanvas");
+    const ctx = canvas.getContext("2d");
     socket.emit("fire", { angle, power });
 };

d2db79da72362c6549965ee84f5b64b402f4a34f
Evan S <105521078+evansolid@users.noreply.github.com>
2026-03-29 16:53:32 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,46 @@
+const socket = io("http://YOUR_PI_IP:3000"); // change this
+
+let myId;
+let players = {};
+
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+socket.on("init", (data) => {
+    myId = data.id;
+    players = data.players;
+    draw();
+});
+
+socket.on("playerJoined", (data) => {
+    players[data.id] = data.player;
+});
+
+socket.on("playerLeft", (id) => {
+    delete players[id];
+});
+
+socket.on("turnResult", (updatedPlayers) => {
+    players = updatedPlayers;
+    draw();
+});
+
+function sendAction() {
+    const angle = parseFloat(document.getElementById("angle").value);
+    const power = parseFloat(document.getElementById("power").value);
+
+    socket.emit("submitAction", { angle, power });
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let id in players) {
+        let p = players[id];
+
+        ctx.beginPath();
+        ctx.arc(p.x, p.y, 10, 0, Math.PI * 2);
+        ctx.fillStyle = (id === myId) ? "blue" : "red";
+        ctx.fill();
+    }
+}

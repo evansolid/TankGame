f18dfaffc78a21ded1dcbbe10a08dc81c75b2caa
Evan S <105521078+evansolid@users.noreply.github.com>
2026-03-29 18:00:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,46 +1,9 @@
-const socket = io("https://usgs-apartment-once-donald.trycloudflare.com");
+const socket = io("https://vacuum-authorities-transportation-sculpture.trycloudflare.com");
 
-let myId;
-let players = {};
+socket.on("connect", () => console.log("Connected:", socket.id));
 
-const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
-
-socket.on("init", (data) => {
-    myId = data.id;
-    players = data.players;
-    draw();
-});
-
-socket.on("playerJoined", (data) => {
-    players[data.id] = data.player;
-});
-
-socket.on("playerLeft", (id) => {
-    delete players[id];
-});
-
-socket.on("turnResult", (updatedPlayers) => {
-    players = updatedPlayers;
-    draw();
-});
-
-function sendAction() {
-    const angle = parseFloat(document.getElementById("angle").value);
-    const power = parseFloat(document.getElementById("power").value);
-
-    socket.emit("submitAction", { angle, power });
-}
-
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    for (let id in players) {
-        let p = players[id];
-
-        ctx.beginPath();
-        ctx.arc(p.x, p.y, 10, 0, Math.PI * 2);
-        ctx.fillStyle = (id === myId) ? "blue" : "red";
-        ctx.fill();
-    }
-}
+document.getElementById("fire").onclick = () => {
+    const angle = document.getElementById("angle").value;
+    const power = document.getElementById("power").value;
+    socket.emit("fire", { angle, power });
+};
